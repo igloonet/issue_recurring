0ccbe87fd814593a8b8c52bb8f971d45c88fbdd0
Kepi <kepi@igloonet.cz>
2022-02-01 23:02:13 +0100

    [patch] Compatibility with tags plugin as suggested in #30
    https://it.michalczyk.pro/issues/30

--- lib/issue_recurring/issue_patch.rb
+++ lib/issue_recurring/issue_patch.rb
@@ -1,41 +1,34 @@
 module IssueRecurring
   module IssuePatch
-    module CopyFromWithRecurrences
-      def copy_from(arg, options={})
-        super
-
+    extend ActiveSupport::Concern
+    included do
+      include InstanceMethods
+      has_many :recurrences, class_name: 'IssueRecurrence', dependent: :destroy
+      belongs_to :recurrence_of, class_name: 'Issue'
+      has_many :recurrence_copies, class_name: 'Issue', foreign_key: 'recurrence_of_id',
+        dependent: :nullify
+      validates :recurrence_of, associated: true, unless: -> { recurrence_of == self }
+      after_destroy :substitute_if_last_issue
+      alias_method :copy_from_without_recurrences, :copy_from
+      alias_method :copy_from, :copy_from_with_recurrences
+    end
+    module InstanceMethods
+      def copy_from_with_recurrences(arg, options={})
+        copy_from_without_recurrences(arg, options)
         unless options[:skip_recurrences]
           self.recurrence_of = nil
-
           if Setting.plugin_issue_recurring[:copy_recurrences]
             self.recurrences = @copied_from.recurrences.map(&:dup)
           end
         end
-
         self
       end
-    end
-
-    Issue.class_eval do
-      prepend CopyFromWithRecurrences
-
-      has_many :recurrences, class_name: 'IssueRecurrence', dependent: :destroy
-
-      belongs_to :recurrence_of, class_name: 'Issue'
-      has_many :recurrence_copies, class_name: 'Issue', foreign_key: 'recurrence_of_id',
-        dependent: :nullify
-
-      validates :recurrence_of, associated: true, unless: -> { recurrence_of == self }
-
-      after_destroy :substitute_if_last_issue
-
       def substitute_if_last_issue
         return if self.recurrence_of.blank?
         r = self.recurrence_of.recurrences.find_by(last_issue: self)
         return if r.nil?
         r.update!(last_issue: r.issue.recurrence_copies.last)
       end
-
       def default_reassign
         self.assigned_to = nil
         default_assign
@@ -43,4 +36,3 @@ module IssueRecurring
     end
   end
 end
-
